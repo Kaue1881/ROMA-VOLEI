ec3614d5983920c1c04bd2675bf604021fb01ab4
Kaue1881 <kauerafael18811881@gmail.com>
2025-05-09 23:20:49 -0300

    html na raiz do projeto

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ const ul = document.querySelector("#lista");
 burguer.addEventListener("click", function () {
   if (burguer.classList.contains("open")) {
     ul.style.display = "none";
-    burguer.style.backgroundImage = "url(../Img/row.png)";
+    burguer.style.backgroundImage = "url(./Img/row.png)";
     burguer.setAttribute("class", "burguer-mobile closed");
   } else {
     ul.style.display = "flex";
-    burguer.style.backgroundImage = "url(../Img/close.png)";
+    burguer.style.backgroundImage = "url(./Img/close.png)";
     burguer.setAttribute("class", "burguer-mobile open");
   }
 });
