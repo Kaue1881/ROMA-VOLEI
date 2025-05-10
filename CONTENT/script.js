358ac15ecb6e8a51b8b21885fd5841e6d7802165
Kaue1881 <kauerafael18811881@gmail.com>
2025-05-09 23:12:48 -0300

    :tada: Initial commit

---
+++ CONTENT/script.js
@@ -0,0 +1,27 @@
+// scroll da paginaY
+const VerMais = document.querySelector(".VerMais");
+const btnSobre = document.querySelector(".sobreLink");
+
+btnSobre.addEventListener("click", function () {
+  window.scrollTo({ top: 650, behavior: "smooth" });
+});
+
+VerMais.addEventListener("click", function () {
+  window.scrollTo({ top: 650, behavior: "smooth" });
+});
+
+// navegação
+const burguer = document.querySelector("#burguer-mobile");
+const ul = document.querySelector("#lista");
+
+burguer.addEventListener("click", function () {
+  if (burguer.classList.contains("open")) {
+    ul.style.display = "none";
+    burguer.style.backgroundImage = "url(../Img/row.png)";
+    burguer.setAttribute("class", "burguer-mobile closed");
+  } else {
+    ul.style.display = "flex";
+    burguer.style.backgroundImage = "url(../Img/close.png)";
+    burguer.setAttribute("class", "burguer-mobile open");
+  }
+});
