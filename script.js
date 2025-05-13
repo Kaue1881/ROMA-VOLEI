641ab31ff710b20c2807bb9964f29bbc9474cb4b
Kaue1881 <kauerafael18811881@gmail.com>
2025-05-12 23:01:04 -0300

    Responsividade ajustada, Elenco Criado

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-// scroll da paginaY
-const VerMais = document.querySelector(".VerMais");
-const btnSobre = document.querySelector(".sobreLink");
-const btnElenco = document.querySelector(".elencoLink");
+const subirAoTopo = document.querySelector('.subir');
 
-btnSobre.addEventListener("click", function () {
-  window.scrollTo({ top: 650, behavior: "smooth" });
-});
+subirAoTopo.addEventListener('click', function(){
+  window.scrollTo({top: 0, behavio: "smooth"});
+})
 
-VerMais.addEventListener("click", function () {
-  window.scrollTo({ top: 650, behavior: "smooth" });
-});
-
-btnElenco.addEventListener("click", function () {
-  window.scrollTo({ top: 1680, behavior: "smooth" });
-});
 // navegação
 const burguer = document.querySelector("#burguer-mobile");
 const ul = document.querySelector("#lista");
@@ -28,4 +18,43 @@ burguer.addEventListener("click", function () {
     burguer.style.backgroundImage = "url(./Img/close.png)";
     burguer.setAttribute("class", "burguer-mobile open");
   }
+});
+
+// retrai a barra de nav quando clicado em algum dos links
+window.addEventListener('resize', function(){
+  const navlink = document.querySelectorAll('.navLink');
+  const burguer = document.querySelector("#burguer-mobile");
+  const ul = document.querySelector("#lista");
+  navlink.forEach(function(link){
+    link.addEventListener('click', function(){
+      ul.style.display = "none";
+      burguer.style.backgroundImage = "url(./Img/row.png)";
+      burguer.setAttribute("class", "burguer-mobile closed");
+    })
+  })
+  if (window.innerWidth > 886) {
+    ul.style.display = "flex";
+    burguer.setAttribute("class", "burguer-mobile closed");
+  } 
+})
+
+const levantador = document.querySelector(".levantador");
+const ponteiros = document.querySelector(".ponteiros");
+const central = document.querySelector(".central");
+const oposto = document.querySelector(".oposto");
+const libero = document.querySelector(".libero");
+
+let jogadoresSelect = document.querySelector("#jogadores");
+
+jogadoresSelect.addEventListener('change', function(){
+  const posicaoSelecionada = this.value;
+  const secoes = document.querySelectorAll('.player-box');
+
+  secoes.forEach(secao => {
+    if(posicaoSelecionada === "todos" || secao.dataset.posicao === posicaoSelecionada){
+      secao.style.display = "flex";
+    } else {
+      secao.style.display = "none";
+    }
+  });
 });
